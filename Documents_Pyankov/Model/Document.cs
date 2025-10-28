1c56ed388840264ffd6fb36c24f1a7f9865bd20b
ВЛАДОС ПАЛТОС <vladipyankov@yandex.ru>
2025-10-28 23:29:04 +0500

    Реализованные модели. Pyankov

---
+++ Documents_Pyankov/Model/Document.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents_Pyankov.Model
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public string Src { get; set; }
+        public string Name { get; set; }
+        public string User { get; set; }
+        public int IdDocument { get; set; }
+        public string Date { get; set; }
+        public int Status { get; set; }
+        public string Direction { get; set; }
+    }
+}
