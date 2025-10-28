e4fb8d8a2c403004bdfd44e5290efb6a480a8cb2
ВЛАДОС ПАЛТОС <vladipyankov@yandex.ru>
2025-10-29 00:28:16 +0500

    Реализованная страница Main. Pyankov

---
+++ Documents_Pyankov/Pages/Main.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Documents_Pyankov.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Documents_Pyankov.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        public Main()
+        {
+            InitializeComponent();
+            CreateUI();
+        }
+
+        public void CreateUI()
+        {
+            Parent.Children.Clear();
+            foreach (DocumentContext item in MainWindow.init.AllDocuments)
+                Parent.Children.Add(new Elements.Item(item));
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            MainWindow.init.Close();
+        }
+
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            MainWindow.init.frame.Navigate(new Pages.Add());
+        }
+    }
+}
