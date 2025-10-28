f615cf8ca1b3ab7d6c3cacf43bca633308dd9bb2
ВЛАДОС ПАЛТОС <vladipyankov@yandex.ru>
2025-10-28 23:34:15 +0500

    Реализованные интерфейсы. Pyankov.

---
+++ Documents_Pyankov/Interfaces/IDocument.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents_Pyankov.Interfaces
+{
+    public interface IDocument
+    {
+        void Save(bool update = false);
+        List<DocumentContext> AllDocuments();
+        void Delete();
+    }
+}
